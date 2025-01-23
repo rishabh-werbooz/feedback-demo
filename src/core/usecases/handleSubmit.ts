8a5b8a82d260b942ea27b9c714010e85a3740466
Rajkumar Jaiswal <rajkumar.jaiswal@werbooz.com>
2025-01-23 20:13:05 +0530

    minor error fixes

--- src/core/usecases/handleSubmit.ts
+++ src/core/usecases/handleSubmit.ts
@@ -8,19 +8,18 @@ interface FormInput {
    * Handles form submission by validating and processing the input data.
    * @param input FormInput
    */
-  export const handleSubmit = (data: FormInput): any => {
+  export const handleSubmit = async (data: FormInput): Promise<any> => {
  
-    fetch("https://webhook.site/9717a3d2-270d-4c87-a6c3-7d0b3b81702e", {
+  const res = await  fetch("https://webhook.site/9717a3d2-270d-4c87-a6c3-7d0b3b81702e", {
       method: "POST",
       mode:"no-cors",
       headers: {
         'Content-Type':"application/json"
       },
       body:JSON.stringify(data)
-    }).then(res => {
-      return res
     })
-  
+    const result = await res.json()
+    return result
     // Further processing such as API call can be added here
 };
   
